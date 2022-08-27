9a185c637fdaea7ec8511078cba9c4c0166df398
Diego Arenas <diegoarenas111@gmail.com>
2022-08-27 16:11:21 -0500

    falta center

--- src/components/mapComponent/map-control.js
+++ src/components/mapComponent/map-control.js
@@ -7,38 +7,60 @@ import { imgIcon } from "./controls/icons/imgIcon";
 import { minimap } from "./controls/minimap";
 import { predios } from "./layers/predios";
 import { perimeter} from "./layers/perimeter";
-import { Modal } from "bootstrap";
+import { dynamicMarker } from "./controls/markers";
+
 
 export var map = L.map("map", {
-  center: [0, 0],
+  center: [10.496444444444, -75.124166666667],
   zoom: 15,
   layers: [dark_nolabels, light_only_labels],
 });
 
+
 //Marker
 const repelon = L.marker([10.496444444444, -75.124166666667], {
-  icon: imgIcon('https://cdn-icons-png.flaticon.com/512/684/684908.png', [41, 41]),
+  icon: imgIcon('marker-icon.28bcaf97.png', [25, 41]),
   }).addTo(map);
+  map.on('move', function () {
+		marker.setLatLng(map.getCenter());
+		//console.log(map.getCenter());
+	});
+	//Dragend event of map for update marker position
+	map.on('dragend', function(e) {
+		var cnt = map.getCenter();
+	        var position = marker.getLatLng();
+		lat = Number(position['lat']).toFixed(5);
+		lng = Number(position['lng']).toFixed(5);
+		//console.log(position);
+		setLeafLatLong(lat, lng);
+		
+	});
+// const center = L.marker([0, 0], {
+//     icon: imgIcon('marker-icon.28bcaf97.png', [25, 41]),
+//     }).addTo(map);
 
-//location REPELON
-map.fitBounds([[repelon.getLatLng().lat, repelon.getLatLng().lng]]).setZoom(15);
+// // //location REPELON
+// map.fitBounds([[repelon.getLatLng().lat, repelon.getLatLng().lng]]).setZoom(15);
 
 //minimap
 minimap.addTo(map);
 
-// Popup
-function popup (features,layers){
-  if(features.properties && feature.properties.codigo)
-   layers.bindpopup("<strong>CODIGO: </strong>" + features.properties.codigo +"<br/>"+"<strong>CODIGO: </strong>"+ features.properties.area)
-};
-
 // GeoJson
-L.geoJson(predios).addTo(map);
 L.geoJson(perimeter).addTo(map);
+L.geoJSON(predios,{
+  style: function (feature) {
+    return {
+      stroke: true,
+      color: "blue", 
+      weight: 1};
+  },
 
-var prediosJS = L.geoJson(predios,{
-    onEachFeature: popup()
+  onEachFeature: function(feature,layer) {
+    layer.bindPopup("<strong>CODIGO: </strong>" + feature.properties.codigo +"<br/>"+"<strong>AREA: </strong>"+ feature.properties.area)
+  },
+  
 }).addTo(map);
+
 
 
 L.control.zoom({ position: "topright" }).addTo(map);
